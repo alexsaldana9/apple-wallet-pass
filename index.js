7e13255825952b722ac776131ff55b24ff8c100c
Alexandra Saldan <alexsaldana9@gmail.com>
2019-01-30 08:17:37 -0500

    added finn image, and fixed datastring  var

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var headers = {
     'content-type': 'multipart/form-data; boundary=----FORM_BOUNDARY',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 };
-// var dataString = '$------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_primaryFields_primary_label"\r\n\r\nname\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_primaryFields_primary_value"\r\n\r\nFinn Shark\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_secondaryFields_secondary_label"\r\n\r\nstatus\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_secondaryFields_secondary_value"\r\n\r\nStudent/Employee\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_level_label"\r\n\r\nId\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_level_value"\r\n\r\n1234567890\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_favorite_label"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_favorite_value"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_backFields_info_label"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_backFields_info_value"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_backgroundColor"\r\n\r\n#f882ee\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_foregroundColor"\r\n\r\n#ffffff\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_logoText"\r\n\r\nMD Card\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_altText"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_format"\r\n\r\nPKBarcodeFormatPDF417\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_message"\r\n\r\n{hashedSerialNumber}\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="thumbnail"; filename=""\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="thumbnail@2x"; filename=""\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_submit"\r\n\r\nRegister\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm"\r\n\r\n0\r\n------FORM_BOUNDARY--\r\n';
 
 //inputs and labels
 var nameLabel = 'Name';
@@ -19,7 +18,49 @@ var barcodeFormat = 'PKBarcodeFormatPDF417';
 var barcodeMessage = '{hashedSerialNumber}';
 
 
-var dataString = '$------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_primaryFields_primary_label"\r\n\r\n+nameLabel+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_primaryFields_primary_value"\r\n\r\n+nameInput+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_secondaryFields_secondary_label"\r\n\r\nstatus\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_secondaryFields_secondary_value"\r\n\r\n+statusInput+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_level_label"\r\n\r\n+idLabel+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_level_value"\r\n\r\n+idInput+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_favorite_label"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_favorite_value"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_backFields_info_label"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_backFields_info_value"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_backgroundColor"\r\n\r\n+backgroundColor+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_foregroundColor"\r\n\r\n#ffffff\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_logoText"\r\n\r\n+logoTextLabel+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_altText"\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_format"\r\n\r\n+barcodeFormat+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_message"\r\n\r\n+barcodeMessage+\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="thumbnail"; filename=""\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="thumbnail@2x"; filename=""\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_submit"\r\n\r\nRegister\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm"\r\n\r\n0\r\n------FORM_BOUNDARY--\r\n';
+var dataString = '$------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_primaryFields_primary_label"\r\n\r\n'
+  + nameLabel
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_primaryFields_primary_value"\r\n\r\n'
+  + nameInput
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_secondaryFields_secondary_label"\r\n\r\n'
+  + statusLabel
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_secondaryFields_secondary_value"\r\n\r\n'
+  + statusInput
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_level_label"\r\n\r\n'
+  + idLabel
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_level_value"\r\n\r\n'
+  + idInput
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_favorite_label"\r\n\r\n'
+  + ''
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_auxiliaryFields_favorite_value"\r\n\r\n'
+  + ''
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_backFields_info_label"\r\n\r\n'
+  + ''
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_structure_backFields_info_value"\r\n\r\n'
+  + ''
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_backgroundColor"\r\n\r\n'
+  + backgroundColor
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_foregroundColor"\r\n\r\n'
+  + '#ffffff'
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_logoText"\r\n\r\n'
+  + logoTextLabel
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_altText"\r\n\r\n'
+  + ''
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_format"\r\n\r\n'
+  + barcodeFormat
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_barcode_message"\r\n\r\n'
+  + barcodeMessage
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="thumbnail"; filename=""'
+  + '\r\nContent-Type: application/octet-stream\r\n\r\n'
+  + ''
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="thumbnail@2x"; filename=""'
+  + '\r\nContent-Type: application/octet-stream\r\n\r\n'
+  + ''
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm_0_submit"\r\n\r\n'
+  + 'Register'
+  + '\r\n------FORM_BOUNDARY\r\nContent-Disposition: form-data; name="psForm"\r\n\r\n'
+  + '0'
+  + '\r\n------FORM_BOUNDARY--\r\n';
 
 var options = {
     url: 'https://www.passsource.com/pass/register.php?templateId=5',
